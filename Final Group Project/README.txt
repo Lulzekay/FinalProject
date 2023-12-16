e8b3dfc9a1aa5b7baac53e7b2dcdf6905a764522
Isaac okai <76550970+Lulzekay@users.noreply.github.com>
2023-12-16 04:18:48 -0500

    Add files via upload

---
+++ Final Group Project/README.txt
@@ -0,0 +1,15 @@
+Team members:
+Darshil Talati (dtalati1)(UID#: 117987881)
+Mustapha Saidykhan (msaidykh)(UID#: 117218589)
+Isaac Okai (iokai)(UID#: 117302080)
+
+App Description:
+This is a sports betting simulator for basketball games. It allows the user to sign up for an
+account and place bets on one of two basketball games. Account info is stored in Mongo and 
+balance is stored in localstorage
+
+API Links:
+None, we used localstorage as the API
+
+Youtube Link:
+https://youtu.be/hvkjBYtto-A
---
+++ Final Group Project/gambling.js
@@ -0,0 +1,162 @@
+"use strict";
+// requires statements
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })
+
+const uri = process.env.MONGO_CONNECTION_STRING;
+const axios = require('axios');
+/* Our database and collection */
+const databaseAndCollection = { db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION };
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("views", path.resolve(__dirname, "templates"));
+
+app.set("view engine", "ejs");
+
+// make sure port number is specified
+
+const portNumber = process.argv[2];
+
+console.log(`Web server started and running at http://localhost:${portNumber}`);
+
+//processing
+app.get("/", (req,res) => {
+    res.render("index");
+});
+
+app.post("/signUpProcessing", (req,res) => {
+    (async () => {
+		const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+		try {
+			await client.connect();
+			let name = req.body.name;
+			let email = req.body.email;
+			let password = req.body.password;
+			let obj = {name: name, email: email, password: password};
+
+            (async () => {
+                const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+        
+                try {
+                    await client.connect();
+                    let filter = {email:email};
+
+                    const result = await client.db(databaseAndCollection.db)
+                        .collection(databaseAndCollection.collection)
+                        .findOne(filter);
+        
+                    if (result) {
+						// if email is already in use then re render the page
+                        res.render("index");
+                    } else {
+						const recaptchaResponse = req.body['g-recaptcha-response'];
+    					const secretKey = "6LfDVTMpAAAAADKIVtgycSIVZYIO0KlZG713j6ky";
+
+						const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaResponse}`;
+
+
+						const recaptchaResult = await axios.post(verifyURL);
+						if (!recaptchaResult.data.success) {
+							// reCAPTCHA verification failed
+							return res.render("index", { error: "reCAPTCHA verification failed" });
+						}
+			
+						
+						// render betting page if sign up was successful
+						await client.db(databaseAndCollection.db)
+                        .collection(databaseAndCollection.collection)
+                        .insertOne(obj);
+						let renderObj = {email:`<input id="email" type="text" name="email" value="${email}" readonly>`};
+                        res.render("bettingForm", renderObj);
+                    }
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    await client.close();
+                }
+            })();
+		} catch (e) {
+			console.error(e);
+		} finally {
+			await client.close();
+		}
+	})();
+});
+
+app.post("/loginProcessing", (req,res) => {
+    (async () => {
+		const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+		try {
+			await client.connect();
+			let email = req.body.loginEmail;
+			let password = req.body.loginPassword;
+			let filter = {email: email, password: password};
+
+            const result = await client.db(databaseAndCollection.db)
+            .collection(databaseAndCollection.collection)
+            .findOne(filter);
+
+            if (result) {
+				// render betting page if email and password is found
+                let obj = {email:`<input id="email" type="text" name="email" value="${email}" readonly>`};
+                res.render("bettingForm", obj);
+            } else {
+                res.render("index");
+            }
+		} catch (e) {
+			console.error(e);
+		} finally {
+			await client.close();
+		}
+
+	})();
+});
+
+app.get("/bettingProcessing", (req, res) => {
+	let pickedTeam = `<span id="team1">${req.query.betGame}</span>`;
+	let email = `<input id="email" type="text" name="email" readonly value="${req.query.email}">`;
+	let balance = `<input id="balance" type="number" name="balance" readonly value="${req.query.balance}">`;
+	let betAmount = `<span id="betAmount">${req.query.betAmount}</span>`;
+	let team1Score = `<span id="team1Score">${Math.round(Math.random() * 50 + 80)}</span>`;
+	let team2Score = `<span id="team2Score">${Math.round(Math.random() * 50 + 80)}</span>`;
+	
+	let team2 = "";
+	if (req.query.betGame === "Pistons") {
+		team2 = `<span id="team2">Hawks</span>`;
+	} else if (req.query.betGame === "Hawks") {
+		team2 = `<span id="team2">Pistons</span>`;
+	} else if (req.query.betGame === "Warriors") {
+		team2 = `<span id="team2">Cavs</span>`;
+	} else {
+		team2 = `<span id="team2">Warriors</span>`;
+	}
+
+	let obj = {email:email, balance:balance, betAmount:betAmount, team1:pickedTeam, team1Score:team1Score,
+	team2:team2, team2Score:team2Score};
+
+	res.render("bettingProcessing", obj);
+});
+
+app.listen(portNumber);
+
+process.stdin.setEncoding("utf8");
+process.stdout.write("Stop to shutdown the server: ");
+process.stdin.on('readable', () => {
+	let dataInput = process.stdin.read();
+	if (dataInput !== null) {
+		let command = dataInput.trim();
+		if (command === "stop") {
+			console.log("Shutting down the server");
+			process.exit(0);
+		} else {
+			console.log(`Invalid command: ${command}`);
+		}
+	}
+	process.stdout.write("Stop to shutdown the server: ");
+	process.stdin.resume();
+});
